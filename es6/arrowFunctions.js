f50e07ac29ac9c98943c647b513f31842aad543a
Deepshikha Banerjee <deepshikhabanerjee4@gmail.com>
2019-12-21 15:24:05 +0530

    Fat arrow funciton

---
+++ es6/arrowFunctions.js
@@ -0,0 +1,16 @@
+/**
+ * Challenge
+ * 
+ * Rewrite the function assigned to the variable magic which returns a new Date() 
+ * to use arrow function syntax. Also make sure nothing is defined using the keyword var.
+ * 
+ */
+
+/* var magic = function () {
+    "use strict";
+    return new Date();
+}; */
+
+
+
+const magic = () => new Date();
