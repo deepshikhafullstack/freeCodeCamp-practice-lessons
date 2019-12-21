626efdc768ee80e02d7ac41c161ccf26a0e628bc
Deepshikha Banerjee <deepshikhabanerjee4@gmail.com>
2019-12-21 17:02:40 +0530

    Destruring nested objects

--- es6/objectDestructuring.js
+++ es6/objectDestructuring.js
@@ -20,7 +20,7 @@ const HIGH_TEMPERATURES = {
   console.log(tomorrow); // should be 80
 
   
-  
+
 const user = {
     name: 'Shouvik',
     age: 31,
@@ -33,7 +33,45 @@ const user = {
  * @param {Object} user
  * @returns {string} 
  */
-const getUserInfo = ({ name, age, phone, experties }) => name + ' is ' + age + ' years old. He is expert in ' + experties[0] + '. His contact number is ' + phone;
+const getUserInfo = ({ name: teamMember, age, phone, experties }) => teamMember + ' is ' + age + ' years old. He is expert in ' + experties[0] + '. His contact number is ' + phone;
 
 console.log(getUserInfo(user));
 
+/**
+ * Destructuring Assignment to Assign Variables from Objects
+ * 
+ */
+
+const HIGH_TEMPERATURES = {
+    yesterday: 75,
+    today: 77,
+    tomorrow: 80
+  };
+  
+  // change code below this line
+    
+  const {today:highToday,tomorrow:highTomorrow} = HIGH_TEMPERATURES;
+  
+  // change code above this line
+  
+  console.log(yesterday) // should be not defined
+  console.log(highToday); // should be 77
+  console.log(highTomorrow); // should be 80
+  
+
+  const LOCAL_FORECAST = {
+    yesterday: { low: 61, high: 75 },
+    today: { low: 64, high: 77 },
+    tomorrow: { low: 68, high: 80 }
+  };
+  
+  // change code below this line
+    
+  const {today:{low:lowToday, high: highToday}} = LOCAL_FORECAST;
+  // change code above this line
+  
+  console.log(lowToday); // should be 64
+  console.log(highToday); // should be 77
+ 
+  
+  
