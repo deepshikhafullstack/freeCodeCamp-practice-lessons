77a08292ed8e55dc64fa9279234db0733b5a3b12
Deepshikha Banerjee <deepshikhabanerjee4@gmail.com>
2019-12-21 18:49:20 +0530

    More on destructuring

--- es6/objectDestructuring.js
+++ es6/objectDestructuring.js
@@ -72,6 +72,25 @@ const HIGH_TEMPERATURES = {
   
   console.log(lowToday); // should be 64
   console.log(highToday); // should be 77
- 
+
+/**
+ * Destructuring Assignment to Pass an Object as a Function's Parameters
+ */
+  const stats = {
+    max: 56.78,
+    standard_deviation: 4.34,
+    median: 34.54,
+    mode: 23.87,
+    min: -0.75,
+    average: 35.85
+  };
   
+  // change code below this line
+  const half = ({max,min}) => (max + min) / 2.0; // use function argument destructuring
+  // change code above this line
+  
+  console.log(stats); // should be object
+  console.log(half(stats)); // should be 28.015
+  
+ 
   
